35aae69ac4f160056418bd108128d32379126046
zachandbro <zachandbro@gmail.com>
2017-12-04 15:43:35 -0500

    changes to task information

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
+<<<<<<< Updated upstream
     $('#One').modal();
     $('#Two').modal();
     $('#Three').modal();
  
  });
+=======
+    $('#One').modal({dismissible: true});
+    $('#Two').modal({dismissible: true});
+    $('#Three').modal({dismissible: true});
+
+ });
+>>>>>>> Stashed changes
