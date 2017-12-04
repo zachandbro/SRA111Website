6f8f4cd0b9a253a192e939e8fff885c6e8299d18
zachandbro <zachandbro@gmail.com>
2017-12-04 16:19:30 -0500

    added ultype style for bullet points

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
 
-<<<<<<< Updated upstream
-    $('#One').modal();
-    $('#Two').modal();
-    $('#Three').modal();
- 
- });
-=======
     $('#One').modal({dismissible: true});
     $('#Two').modal({dismissible: true});
     $('#Three').modal({dismissible: true});
 
  });
->>>>>>> Stashed changes
